b53d7d7f5d8c5f01356bb869c68d5a6d9f27fedf
Sadia Sakharkar <sakharkarsadia@gmail.com>
2025-03-17 19:41:56 +0530

    Practical01-07

--- LAB PRACTICE/Reversestring.py
+++
@@ -1,9 +0,0 @@
-# Take input from the user
-user_input = input("Enter a string: ")  
-
-# Reverse the string and join characters with a comma
-reversed_string = user_input[::-1]  # This reverses the string
-result = ",".join(reversed_string)  # This adds a comma between each character
-
-# Print the final result
-print(result)
